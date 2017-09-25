e8f2aede0c39b4fe67a9e4509342aad6ae3dfadc
Kevin Burleigh <klb@kindling>
2017-09-25 13:35:54 -0500

    add serices/ and install script

---
+++ services/code/kprog1.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+counter = 0
+File::open('/home/ubuntu/log1.txt', 'a') do |fh|
+  loop do
+    fh.write("#{Time.now} #{counter}\n")
+    fh.flush
+    counter += 1
+    sleep(1.0)
+  end
+end
---
+++ services/install_services.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+services_source_dirname = ARGV[0]
+services_target_dirname = ARGV[1]
+
+Dir.foreach(services_source_dirname) do |filename|
+  puts("filename = #{filename}")
+  if matches = filename.match(%r{^(.*/)*(?<service_name>.*)\.service$})
+    service_name = matches[:service_name]
+    puts("  service_name = #{service_name}")
+
+    FileUtils.cp(filename, services_target_dirname)
+    system("systemctl enable #{service_name}")
+    system("systemctl restart #{service_name}")
+  end
+end
