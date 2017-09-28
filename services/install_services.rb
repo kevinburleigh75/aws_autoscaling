ea26dc008bc921b2e38ea435cb8cf072b1335162
Kevin Burleigh <klb@kindling>
2017-09-28 08:36:22 -0500

    enable specified services only

--- services/install_services.rb
+++ services/install_services.rb
@@ -2,8 +2,28 @@
 
 require 'fileutils'
 
-services_source_dirname = ARGV[0]
-services_target_dirname = ARGV[1]
+service_prefix          = ARGV[0]
+services_source_dirname = ARGV[1]
+services_target_dirname = ARGV[2]
+enabled_services        = Array(ARGV[3..-1])
+
+##
+## Clean up any existing services.
+##
+
+Dir.foreach(services_source_dirname) do |filename|
+  puts("filename = #{filename}")
+  if matches = filename.match(%r{^(.*/)*(?<service_name>.*)\.service$})
+    service_name = matches[:service_name]
+    next unless service_name.match(%r/^#{service_prefix}/)
+    system("systemctl disable #{service_name}")
+    FileUtils.rm_f(filename)
+  end
+end
+
+##
+## Install all services.
+##
 
 Dir.foreach(services_source_dirname) do |filename|
   puts("filename = #{filename}")
@@ -12,7 +32,14 @@ Dir.foreach(services_source_dirname) do |filename|
     puts("  service_name = #{service_name}")
 
     FileUtils.cp(filename, services_target_dirname)
-    system("systemctl enable #{service_name}")
-    system("systemctl restart #{service_name}")
   end
+end
+
+##
+## Enable specified services.
+##
+
+enabled_services.each do |service_name|
+  system("systemctl enable #{service_name}")
+  system("systemctl restart #{service_name}")
 end
