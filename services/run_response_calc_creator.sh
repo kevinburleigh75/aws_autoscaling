87ac424f88cd0561032bba560c1fe9e9aa77e0c0
Kevin Burleigh <klb@kindling>
2017-10-19 09:40:26 -0500

    add bundle install

--- services/run_response_calc_creator.sh
+++ services/run_response_calc_creator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
-. .profile
+. ~/.profile
 
 cd /home/ubuntu/aws_autoscaling/ruby
+RAILS_ENV=production bundle install
 RAILS_ENV=production bundle exec rake demo1:response_calc[1125ed96-3f2f-49dc-b8e2-58302de77777,0.1,1,0]
