7e0e29162fa9f7bf4b4f5054c0ecb164984401fe
Kevin Burleigh <klb@kindling>
2017-11-07 10:39:25 -0600

    fix typo

--- initialize.sh
+++ initialize.sh
@@ -3,7 +3,7 @@
 sudo -H -i -u ubuntu bash -c "aws configure set s3.signature_version s3v4"
 sudo -H -i -u ubuntu bash -c "aws s3api get-object --bucket secrets-exper --key secrets .secrets"
 
-sudo -H -i -u ubunto bash -c "/home/ubunutu/primary_repo/create_awsinfo.sh"
+sudo -H -i -u ubunto bash -c "/home/ubuntu/primary_repo/create_awsinfo.sh"
 sudo -H -i -u ubuntu bash -c "aws configure set region $AWS_REGION"
 
 (cd /home/ubuntu/primary_repo/services; /home/ubuntu/.rvm/rubies/ruby-2.3.1/bin/ruby ./install_services.rb kevin . /etc/systemd/system/)
