b39bb0765400114648e3bb8f11b0cf24b32007bb
Kevin Burleigh <klb@kindling>
2017-11-07 08:54:35 -0600

    adjust repo name

--- services/run_reporter.sh
+++ services/run_reporter.sh
@@ -2,6 +2,6 @@
 
 . ~/.profile
 
-cd /home/ubuntu/aws_autoscaling/ruby
+cd /home/ubuntu/primary_repo/ruby
 RAILS_ENV=production bundle install
 RAILS_ENV=production bundle exec rake demo2:report[1125ed96-3f2f-49dc-b8e2-58302de99999,0.25,1,0]
