27eedde5e333977a4f37694d89351013085bc78d
Kevin Burleigh <klb@kindling>
2018-02-15 10:15:01 -0600

    bump to (5)

--- ruby/app/controllers/tasks_controller.rb
+++ ruby/app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   def task1
     sleep(0.05)
-    render plain: 'well that was fun (4)'
+    render plain: 'well that was fun (5)'
   end
 
   def task2
