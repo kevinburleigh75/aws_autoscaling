728d25bc8f63e0c11f7316687a10880cbd83283e
Kevin Burleigh <klb@kindling>
2018-03-07 06:07:13 -0600

    add more env vars and output them

--- ruby/app/controllers/tasks_controller.rb
+++ ruby/app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   def task1
     sleep(0.05)
-    render plain: 'well that was fun (6)'
+    render plain: "InstanceId: #{ENV['AWS_INSTANCE_ID']} LcName: #{ENV['AWS_ASG_LC_NAME']} LcImageId: #{ENV['AWS_ASG_LC_IMAGE_ID']}"
   end
 
   def task2
