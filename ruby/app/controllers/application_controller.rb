5dbf2473de8951a9379396519b0bfb4a49adcc24
Kevin Burleigh <klb@kindling>
2018-03-21 15:09:39 -0500

    add after_action for healthcheck file

--- ruby/app/controllers/application_controller.rb
+++ ruby/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  after_action :touch_healthcheck_file
+
+  private
+
+  def touch_healthcheck_file
+    filename = Rails.root.join('tmp','status.txt').to_s
+    FileUtils.touch(filename)
+  end
 end
