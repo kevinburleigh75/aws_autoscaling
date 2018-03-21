a6c9bb716382dce1014074ac04d71bc9e0059533
Kevin Burleigh <klb@kindling>
2018-03-21 14:40:26 -0500

    add separate healthcheck ping server

--- services/run_ping_server.sh
+++ services/run_ping_server.sh
@@ -5,7 +5,7 @@
 cd /home/ubuntu
 mkdir -p ping_server
 cd ping_server
-touch ping
+echo InstanceId: $AWS_INSTANCE_ID LcName: $AWS_ASG_LC_NAME LcImageId: $AWS_ASG_LC_IMAGE_ID > ping
 
 echo "starting ping server..."
 python -m SimpleHTTPServer 8000
