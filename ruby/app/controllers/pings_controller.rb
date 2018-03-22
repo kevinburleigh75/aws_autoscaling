3625bee6ef2f26af69e0ebd07b679f9d79728298
Kevin Burleigh <klb@kindling>
2018-03-22 09:35:22 -0500

    move status file update from ApplicationController after_action to individual controller actions

--- ruby/app/controllers/pings_controller.rb
+++ ruby/app/controllers/pings_controller.rb
@@ -1,5 +1,8 @@
 class PingsController < ApplicationController
   def ping
     render plain: "I'm so alive"
+
+    filename = Rails.root.join('tmp','status.txt').to_s
+    FileUtils.touch(filename)
   end
 end
--- ruby/app/controllers/tasks_controller.rb
+++ ruby/app/controllers/tasks_controller.rb
@@ -2,10 +2,16 @@ class TasksController < ApplicationController
   def task1
     sleep(0.05)
     render plain: "InstanceId: #{ENV['AWS_INSTANCE_ID']} LcName: #{ENV['AWS_ASG_LC_NAME']} LcImageId: #{ENV['AWS_ASG_LC_IMAGE_ID']}"
+
+    filename = Rails.root.join('tmp','status.txt').to_s
+    FileUtils.touch(filename)
   end
 
   def task2
     sleep(30)
     render plain: 'well that was long'
+
+    filename = Rails.root.join('tmp','status.txt').to_s
+    FileUtils.touch(filename)
   end
 end
