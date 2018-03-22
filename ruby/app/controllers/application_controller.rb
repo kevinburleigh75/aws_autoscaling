255351fd816233389ce34cfcc10cc107d8c7c056
Kevin Burleigh <klb@kindling>
2018-03-22 09:38:57 -0500

    forgot to commit this file

--- ruby/app/controllers/application_controller.rb
+++ ruby/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  after_action :touch_healthcheck_file
+  # after_action :touch_healthcheck_file
 
-  private
+  # private
 
-  def touch_healthcheck_file
-    filename = Rails.root.join('tmp','status.txt').to_s
-    FileUtils.touch(filename)
-  end
+  # def touch_healthcheck_file
+  #   filename = Rails.root.join('tmp','status.txt').to_s
+  #   FileUtils.touch(filename)
+  # end
 end
